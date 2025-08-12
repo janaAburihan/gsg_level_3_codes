b7abada0cb772971cedb8531789584954e427b45
Jana Aburihan <105488402+janaAburihan@users.noreply.github.com>
2025-08-12 19:17:40 +0300

    enhance readability

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@ int n;
 
 void recur(int x, vector<string> cur){
     if (x == n){
-        for(string c: cur)cout<<c<<" ";
+        for(string c: cur) {
+            cout<<c<<" ";
+        }
         cout<<endl;
         return;
     }
   
     string act[3] = {"Running", "Swimming", "Football"};
+    
     for (int i=0; i<3; i++){
         if(!cur.size() || cur.back() != act[i]){
             cur.push_back(act[i]);
